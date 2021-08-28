4cd20be228aab97d1c8f7affc7ae1b4e64c2c2a2
Andrew Schreiber <andrew.schreiber1@gmail.com>
2021-08-28 01:23:16 -0700

    feat: working json generator

--- ipfs/jsonGenerator.js
+++ ipfs/jsonGenerator.js
@@ -1,7 +1,6 @@
 const _ = require("lodash");
 const fs = require("fs");
 var path = require("path");
-const { split } = require("lodash");
 
 // To get text data:
 // Install IPFS, run daemon
@@ -9,135 +8,76 @@ const { split } = require("lodash");
 // ipfs ls <FOLDERCID> > ImageHashes.txt
 // Ideally we use pinata so its faster, took ~20m to download 700mb
 
-// const {
-// imageData,
-// } = require("./ImageFolders/Final_GeneratedImages_1629966248697");
+const projectPrefix = "Neapolitan";
+const projectFolderCID = "QmUJ8R4fgxEicPiRheUf9h34EQrYN8xMvwJQ2HqRnLYiYd";
 
-const projectPrefix = "Neapolitan-";
-const projectFolderCID = ""
+var imageHashArr = [];
 
 const filepath = path.join(__dirname, "ImageHashes.txt");
-
 var rawImageHashes = fs.readFileSync(filepath).toString().split("\n");
-// Example:
-// QmYGb8UCtLyamhpSGxkbWtnMNGHxJbsp9bLumgPueqmcpa 220559 Neapolitan-0000-[Vanilla][Banana][Apple].jpg
 
-const imageHashArr = [];
+// Example imageHash:
+// QmcXn42K4ALzZnaRUYS6CsWcuSnKg5faSnYn1XnSrDf33v 220437 Neapolitan-0006-[Dragonfruit][Lime][PeanutButter].jpg
 
 for (const imageHash of rawImageHashes) {
   if (!imageHash.includes(projectPrefix)) continue;
-  const splitArr = imageHash.split(" ");
-  const 
-  imageHashArr.push({ hash: splitArr[0], name: splitArr[2] });
-}
 
-console.log(imageHashArr[0]);
+  const splitHashArr = imageHash.split(" ");
+  const hash = splitHashArr[0];
+  const rawName = splitHashArr[2];
 
-let remapping = {
-  CrazyHair: "Curly Hair",
-  NerdGlasses: "Genius Glasses",
-  "3DGlasses": "3D Glasses",
-  VR: "VR",
-  "Do-Rag": "Do-Rag",
-  // "NerdGlasses": "Genius Glasses",
-};
+  // rawName:Neapolitan-0006-[Dragonfruit][Lime][PeanutButter].jpg
+  const splitRawNameArr = rawName.split("-");
+  const index = splitRawNameArr[1];
+  // index: 0006
 
-let template = {
-  name: "Neopolitan #", //+ number
-  description: "Neopolitan #", //+ number
-  image: "https://rebelpunks.s3.amazonaws.com/", //+ hash //TODO: replace with base url of whereever the images are uploaded, S3 for now...
-  image24px: "https://ipfs.io/ipfs/",
-  attributes: [],
-};
+  const rawAtts = splitRawNameArr[2];
+  // rawAtts: [Dragonfruit][Lime][PeanutButter].jpg
+  const attNamesArr = rawAtts
+    .split("]")
+    .filter((att) => att.includes("["))
+    .map((att) =>
+      // Remove left bracket, add spaces between capitals
+      att
+        .replace("[", "")
+        .replace(/([A-Z])/g, " $1")
+        .trim()
+    );
+  // attNamesArr: ['Dragonfruit', 'Lime', 'Peanut Butter']
+  const attributes = [
+    { trait_type: "Left Flavor", value: attNamesArr[0] },
+    { trait_type: "Center Flavor", value: attNamesArr[1] },
+    { trait_type: "Right Flavor", value: attNamesArr[2] },
+  ];
 
-let templateLeftFlavor = {
-  trait_type: "Left Flavor",
-  value: "",
-};
+  const blob = {
+    name: `${projectPrefix} #${index}`,
+    description: `${projectPrefix} #${index}`,
+    image: `https://ipfs.io/ipfs/${projectFolderCID}/${rawName}`,
+    attributes: attributes,
+  };
+  /* Example blob: {
+    "name": "Neapolitan #0006",
+    "description": "Neapolitan #0006",
+    "image": "https://ipfs.io/ipfs/QmUJ8R4fgxEicPiRheUf9h34EQrYN8xMvwJQ2HqRnLYiYd/Neapolitan-0006-[Dragonfruit][Lime][PeanutButter].jpg",
+    "attributes": [
+      { "trait_type": "Left Flavor", "value": "Dragonfruit" },
+      { "trait_type": "Center Flavor", "value": "Lime" },
+      { "trait_type": "Right Flavor", "value": "Peanut Butter" }
+    ]
+  }*/
 
-let templateCenterFlavor = {
-  trait_type: "Center Flavor",
-  value: "",
-};
-let templateRightFlavor = {
-  trait_type: "Center Flavor",
-  value: "",
-};
-
-let abvTypeToAttribute = {
-  MN: ["Masculine", "Alien"],
-  FN: ["Feminine", "Alien"],
-};
-
-for (let x = 0; x < imageData.length; x++) {
-  // for(let x =0; x < 20; x++){
-  let tempTemplate = _.cloneDeep(template);
-  const thisPunkData = punkData[x];
-
-  const punkAttributes = thisPunkData[1];
-
-  const punkInt = parseInt(thisPunkData[0]);
-  tempTemplate["name"] = `${tempTemplate["name"]}${punkInt}`;
-  tempTemplate["description"] = `${tempTemplate["description"]}${punkInt}`;
-
-  //TODO HERE
-  let imageName = `rebelpunk-${thisPunkData[0]}-`;
-  for (let p = 0; p < punkAttributes.length; p++) {
-    imageName = `${imageName}%5B${punkAttributes[p]}%5D`;
-  }
-  imageName = `${imageName}.png`;
-
-  tempTemplate["image"] = `${tempTemplate["image"]}${imageName}`;
-  tempTemplate["image24px"] = `${tempTemplate["image24px"]}${thisPunkData[2]}`;
-
-  //Parse // '[MN][Cigarette][Earring][NerdGlasses]'
-  for (let y = 0; y < punkAttributes.length; y++) {
-    if (y === 0) {
-      /** Add Type Data */
-      let tempType1 = _.cloneDeep(templateBase);
-      tempType1.value = abvTypeToAttribute[punkAttributes[0]][0];
-      tempTemplate.attributes.push(tempType1);
-
-      let tempType2 = _.cloneDeep(templateTone);
-      tempType2.value = abvTypeToAttribute[punkAttributes[0]][1];
-      tempTemplate.attributes.push(tempType2);
-    } else {
-      /** Add Attributes */
-      let tempAccessory1 = _.cloneDeep(templateAccessory);
-
-      tempAccessory1.value = punkAttributes[y];
-      //Hotswap name if in remapping object.
-      if (remapping[punkAttributes[y]] != undefined) {
-        tempAccessory1.value = remapping[punkAttributes[y]];
-      } else {
-        //TODO: Split ON Capital Letters and add spaces to attributes.
-        let spacesBeforeCapitols = punkAttributes[y]
-          .replace(/([A-Z])/g, " $1")
-          .trim();
-        if (spacesBeforeCapitols != punkAttributes[y]) {
-          tempAccessory1.value = spacesBeforeCapitols;
-        }
-      }
-
-      tempTemplate.attributes.push(tempAccessory1);
-    }
-  }
-
-  let tempAttributeCount1 = _.cloneDeep(templateAttributeCount);
-  tempAttributeCount1.value = `${punkAttributes.length - 1}`;
-  tempTemplate.attributes.push(tempAttributeCount1);
-
-  // fs.writeFile(`/ipfs/UriJsonTest/${punkInt}-test.json`, JSON.stringify(tempTemplate), {flag: 'r+'}, err => {
-  fs.writeFile(
-    `ipfs/ipfsURIJSON_FromLocalFileName/${punkInt}.json`,
-    JSON.stringify(tempTemplate, null, 2),
+  // console.log(JSON.stringify(blob));
+  fs.writeFileSync(
+    `ipfs/generatedJson/${index}.json`,
+    JSON.stringify(blob, null, 2),
     (err) => {
       if (err) {
         console.error(err);
         return;
       }
-      //file written successfully
     }
   );
-  // console.log(tempTemplate);
 }
+
+console.log("JSON Generation complete for count", rawImageHashes.length);
